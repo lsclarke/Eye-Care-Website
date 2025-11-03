5ce17c6f391163709c590098c3dca8aec662c822
Lenard Clarke <57269847+lsclarke@users.noreply.github.com>
2025-11-03 17:45:44 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,188 @@
+
+//JQuery
+
+$(".nav-link.item.one").on("mouseover", function () {
+    $(".nav-link.item.one").css("color", "white");
+    $(".nav-link.item.one").animate({
+                fontSize: "1.55pc"
+            });
+});
+
+$(".nav-link.item.one").on("mouseleave", function () {
+    $(".nav-link.item.one").css("color", "blanchedalmond");
+    $(".nav-link.item.one").animate({
+                fontSize: "1.4pc"
+    });
+});
+ 
+$(".nav-link.item.two").on("mouseover", function () {
+    $(".nav-link.item.two").css("color", "white");
+    $(".nav-link.item.two").animate({
+                fontSize: "1.55pc"
+            });
+});
+
+$(".nav-link.item.two").on("mouseleave", function () {
+    $(".nav-link.item.two").css("color", "blanchedalmond");
+    $(".nav-link.item.two").animate({
+                fontSize: "1.4pc"
+    });
+});
+
+$(".nav-link.item.three").on("mouseover", function () {
+    $(".nav-link.item.three").css("color", "white");
+    $(".nav-link.item.three").animate({
+                fontSize: "1.55pc"
+            });
+});
+
+$(".nav-link.item.three").on("mouseleave", function () {
+    $(".nav-link.item.three").css("color", "blanchedalmond");
+    $(".nav-link.item.three").animate({
+                fontSize: "1.4pc"
+    });
+});
+
+$(".nav-link.item.four").on("mouseover", function () {
+    $(".nav-link.item.four").css("color", "white");
+    $(".nav-link.item.four").animate({
+                fontSize: "1.55pc"
+            });
+});
+
+$(".nav-link.item.four").on("mouseleave", function () {
+    $(".nav-link.item.four").css("color", "blanchedalmond");
+    $(".nav-link.item.four").animate({
+                fontSize: "1.4pc"
+    });
+});
+
+$(".nav-link.item.five").on("mouseover", function () {
+    $(".nav-link.item.five").css("color", "white");
+    $(".nav-link.item.five").animate({
+                fontSize: "1.55pc"
+            });
+});
+
+$(".nav-link.item.five").on("mouseleave", function () {
+    $(".nav-link.item.five").css("color", "blanchedalmond");
+    $(".nav-link.item.five").animate({
+                fontSize: "1.4pc"
+    });
+});
+
+$(".nav-link.item.six").on("mouseover", function () {
+    $(".nav-link.item.six").css("color", "white");
+    $(".nav-link.item.six").animate({
+                fontSize: "1.55pc"
+            });
+});
+
+$(".nav-link.item.six").on("mouseleave", function () {
+    $(".nav-link.item.six").css("color", "blanchedalmond");
+    $(".nav-link.item.six").animate({
+                fontSize: "1.4pc"
+    });
+});
+
+
+$(".carousel-card.one").on("mouseover", function () {
+    $(".carousel-card.one").css("color", "white");
+    $('.carousel-card.one img').animate({
+    width: 190,
+    height: 110,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.one").on("mouseleave", function () {
+    $(".carousel-card.one").css("color", "blanchedalmond");
+    $('.carousel-card.one img').animate({
+    width: 170,
+    height: 100,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.two").on("mouseover", function () {
+    $(".carousel-card.two").css("color", "white");
+    $('.carousel-card.two img').animate({
+    width: 190,
+    height: 110,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.two").on("mouseleave", function () {
+    $(".carousel-card.two").css("color", "blanchedalmond");
+    $('.carousel-card.two img').animate({
+    width: 170,
+    height: 100,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.three").on("mouseover", function () {
+    $(".carousel-card.three").css("color", "white");
+    $('.carousel-card.three img').animate({
+    width: 190,
+    height: 110,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.three").on("mouseleave", function () {
+    $(".carousel-card.three").css("color", "blanchedalmond");
+    $('.carousel-card.three img').animate({
+    width: 170,
+    height: 100,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.four").on("mouseover", function () {
+    $(".carousel-card.four").css("color", "white");
+    $('.carousel-card.four img').animate({
+    width: 190,
+    height: 110,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.four").on("mouseleave", function () {
+    $(".carousel-card.four").css("color", "blanchedalmond");
+    $('.carousel-card.four img').animate({
+    width: 170,
+    height: 100,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.five").on("mouseover", function () {
+    $(".carousel-card.five").css("color", "white");
+    $('.carousel-card.five img').animate({
+    width: 190,
+    height: 110,
+    top: 0,
+
+});
+});
+
+$(".carousel-card.five").on("mouseleave", function () {
+    $(".carousel-card.five").css("color", "blanchedalmond");
+    $('.carousel-card.five img').animate({
+    width: 170,
+    height: 100,
+    top: 0,
+
+});
+});
+
